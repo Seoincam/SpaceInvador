8be5e132b5c40333a8b01762315a0ab854f14f25
Seoincam <seoin4715@naver.com>
2025-11-12 14:17:04 +0900

    FEAT: 총알 회수 (#3)
    * feat: 총알 횟수 제한/회수 기능 추가
    
    * feat: 우클릭으로 총알 회수모드로 변경

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,3 +1,4 @@
+using PlayerController;
 using Services;
 using UnityEngine;
 
@@ -5,20 +6,29 @@ namespace DefaultNamespace
 {
     public interface IBullet
     {
-        void Shoot(Vector2 direction);
+        bool CanRetrieve { get; }
+        GameObject GameObject { get; }
+        void Shoot(Vector2 direction, Transform safeArea);
+        void SetRetrieve();
     }
     
-    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer))]
     public class Bullet : MonoBehaviour, IBullet
     {
         [SerializeField] private float speed;
-        [SerializeField] private int wrapCount;
+        [SerializeField] private LayerMask targetLayer;
         
         private const float Bound = 8.5f;
 
         private Rigidbody2D _rb;
+        private SpriteRenderer _renderer;
+
+        private bool _isActive;
         private Vector2 _direction;
-        private int _maxWrapCount;
+        private Transform _safeArea;
+
+        public bool CanRetrieve { get; private set; }
+        public GameObject GameObject => gameObject;
 
         private void Awake()
         {
@@ -26,68 +36,83 @@ namespace DefaultNamespace
             _rb.gravityScale = 0;
             _rb.freezeRotation = true;
             
-            _maxWrapCount = wrapCount;
+            _renderer = GetComponent<SpriteRenderer>();
         }
-
-        private void OnEnable()
-        {
-            wrapCount = _maxWrapCount;
-        }
+        
         private void OnDisable()
         {
             _rb.linearVelocity = Vector2.zero; // movePosition() 기반이라 필요 없지만, 혹시 velocity를 쓸 수 있으니 초기화.
             _direction = Vector2.zero;
+            _isActive = false;
         }
         
         private void FixedUpdate()
         {
             Vector2 nextPos = _rb.position + _direction * (speed * Time.fixedDeltaTime);
-            bool wrapped = false;
-            
-            // X Wrap
-            if (nextPos.x > Bound)
+            nextPos.x = nextPos.x switch
             {
-                nextPos.x = -Bound;
-                wrapped = true;
-            }
-            else if (nextPos.x < -Bound)
+                > Bound => -Bound,
+                < -Bound => Bound,
+                _ => nextPos.x
+            };
+            nextPos.y = nextPos.y switch
             {
-                nextPos.x = Bound;
-                wrapped = true;
-            }
-
-            // Y Wrap
-            if (nextPos.y > Bound)
-            {
-                nextPos.y = -Bound;
-                wrapped = true;
-            }
-            else if (nextPos.y < -Bound)
-            {
-                nextPos.y = Bound;
-                wrapped = true;
-            }
-
-            if (wrapped)
-            {
-                wrapCount--;
-                if (wrapCount < 0)
-                {
-                    GameServices.Spawner.Despawn(gameObject);
-                    return;
-                }
-            }
+                > Bound => -Bound,
+                < -Bound => Bound,
+                _ => nextPos.y
+            };
             
             _rb.MovePosition(nextPos);
         }
 
-        public void Shoot(Vector2 direction)
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!_isActive)
+                return;
+            if ((targetLayer & (1 << other.gameObject.layer)) == 0)
+                return;
+            if (!other.TryGetComponent(out IDamageable damageable))
+                return;
+
+            if (CanRetrieve && other.TryGetComponent(out IPlayerShooter player))
+            {
+                player.Retrieve(this);
+                Despawn();
+                return;
+            }
+            
+            damageable.TakeDamage();
+            Despawn();
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.gameObject.transform == _safeArea)
+            {
+                _isActive = true;
+            }
+        }
+
+        public void Shoot(Vector2 direction,Transform safeArea)
         {
             _direction = direction;
+            _safeArea = safeArea;
             
-            // 회전
-            float angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0, 0, angle);
+            _isActive = false;
+            CanRetrieve = false;
+            _renderer.color = Color.yellow;
+        }
+
+        public void SetRetrieve()
+        {
+            _direction *= -1;
+            CanRetrieve = true;
+            _renderer.color = Color.white;
+        }
+
+        private void Despawn()
+        {
+            GameServices.Spawner.Despawn(gameObject);
         }
     }
 }
--- Assets/Scripts/PlayerController/PlayerShooter.cs
+++ Assets/Scripts/PlayerController/PlayerShooter.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using DefaultNamespace;
 using Services;
 using UnityEngine;
@@ -8,18 +9,26 @@ namespace PlayerController
     {
         void SetAimInput(Vector2 aimInput);
         void TryFire();
+        void TryRetrieve();
+        void Retrieve(IBullet bullet);
     }
     
-    public class PlayerShooter : MonoBehaviour, IPlayerShooter
+    public class PlayerShooter : MonoBehaviour, IPlayerShooter, IDamageable
     {
+        [SerializeField] private int maxBulletCount = 4;
+        [SerializeField] private Transform bulletSafeArea;
+        
         private Camera _cam;
         
         private Vector2 _aimInput;
         private Vector2 _aimDirection;
+        private List<IBullet> _bullets;
 
         private void Awake()
         {
             _cam = Camera.main;
+            
+            _bullets = new List<IBullet>(maxBulletCount);
         }
 
         private void Update()
@@ -36,15 +45,44 @@ namespace PlayerController
 
         public void TryFire()
         {
-            Debug.Log("Try Fire: " + _aimDirection);
+            if (_bullets.Count >= maxBulletCount)
+                return;
             Shoot(_aimDirection);
+        }
+
+        public void TryRetrieve()
+        {
+            if (_bullets.Count <= 0) 
+                return;
+
+            foreach (var bullet in _bullets)
+            {
+                if (!bullet.CanRetrieve)
+                {
+                    bullet.SetRetrieve();
+                    break;
+                }
+            }
+        }
+
+        public void Retrieve(IBullet bullet)
+        {
+            _bullets.Remove(bullet);
+        }
+        
+        public void TakeDamage()
+        {
+            Debug.Log($"Player {gameObject.name} damaged!");
         }
 
         private void Shoot(Vector2 direction)
         {
             var bulletGo = GameServices.Spawner.Spawn("bullet", transform.position);
-            if (bulletGo.TryGetComponent(out IBullet bullet))
-                bullet.Shoot(direction);
+            if (!bulletGo.TryGetComponent(out IBullet bullet)) 
+                return;
+            
+            _bullets.Add(bullet);
+            bullet.Shoot(direction, bulletSafeArea);
         }
     }
 }
