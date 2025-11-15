e64369dbf74bca5fa166c64fbf3d0a27eca8c49d
Seoincam <seoin4715@naver.com>
2025-11-15 22:37:19 +0900

    feat: VisibleOnly Mode 추가

--- Assets/Scripts/Attributes/VisibleOnly.cs
+++ Assets/Scripts/Attributes/VisibleOnly.cs
@@ -5,5 +5,18 @@ namespace Attributes
 {
     public class VisibleOnly : PropertyAttribute
     {
+        public Mode VisibleOnlyMode { get; }
+
+        public VisibleOnly(Mode mode = Mode.Always)
+        {
+            VisibleOnlyMode = mode;
+        }
+        
+        public enum Mode
+        {
+            Always,
+            EditModeOnly,
+            PlayModeOnly
+        }
     }
 }
--- Assets/Scripts/Editor/VisibleOnlyDrawer.cs
+++ Assets/Scripts/Editor/VisibleOnlyDrawer.cs
@@ -9,9 +9,20 @@ namespace Editor
     {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            GUI.enabled = false;
-            EditorGUI.PropertyField(position, property, label, true);
-            GUI.enabled = true;
+            var visibleOnly = attribute as VisibleOnly;
+
+            bool canEdit = visibleOnly?.VisibleOnlyMode switch
+            {
+                VisibleOnly.Mode.Always => false,
+                VisibleOnly.Mode.EditModeOnly => Application.isPlaying,
+                VisibleOnly.Mode.PlayModeOnly => !Application.isPlaying,
+                _ => false
+            };
+
+            using (new GuiEnabledScope(canEdit))
+            {
+                EditorGUI.PropertyField(position, property, label, true);
+            }
         }
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
